42d461c97ffbe75739f904b452928684fdf77e7f
pedroping <84201034+pedroping@users.noreply.github.com>
2023-02-05 20:44:50 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,12 @@ export class AppComponent {
   constructor(private _fb: FormBuilder) {}
   ngOnInit() {
     this.invoiceForm = new FormGroup({
-      Rows: new FormArray([this.initRows()], Validators.required),
+      Rows: new FormArray([], Validators.required),
     });
+
+    for (let i = 0; i < 10; i++) {
+      this.addNewRow()
+    }
 
     this.invoiceForm.valueChanges.subscribe((x) => {
       console.log('Form', x.Rows[0], this.formArr.valid, 
@@ -32,8 +36,8 @@ export class AppComponent {
   initRows() {
     return new FormGroup({
       name: new FormControl('', Validators.required),
+      idade: new FormControl(null as any, Validators.required),
       sexo: new FormControl('', Validators.required),
-      idade: new FormControl(null as any, Validators.required)
     });
   }
 
