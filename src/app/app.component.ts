6b0bc2df79ff023230d44463e3dc92c130c618de
pedroping <84201034+pedroping@users.noreply.github.com>
2023-02-05 20:32:04 -0300

    Validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,38 @@
-import { Component } from "@angular/core";
-import { FormGroup, FormArray, FormBuilder, Validators, FormControl } from "@angular/forms";
+import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormArray,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   public invoiceForm: FormGroup;
   constructor(private _fb: FormBuilder) {}
   ngOnInit() {
     this.invoiceForm = new FormGroup({
-      Rows: new FormArray([this.initRows()])
+      Rows: new FormArray([this.initRows()], Validators.required),
+    });
+
+    this.invoiceForm.valueChanges.subscribe((x) => {
+      console.log('Form', x.Rows[0], this.formArr.valid, 
+      this.formArr.controls[0].valid);
     });
   }
 
   get formArr() {
-    return this.invoiceForm.get("Rows") as FormArray;
+    return this.invoiceForm.get('Rows') as FormArray;
   }
 
   initRows() {
     return new FormGroup({
-      name: new FormControl(""),
-      sexo: new FormControl("")
+      name: new FormControl('', Validators.required),
+      sexo: new FormControl('', Validators.required),
     });
   }
 
